a188de10685414ead4a3f671effd6620eb4fefe9
Jerry Gao <gao@blackwright.com>
2019-03-17 19:07:56 -0400

    clean up Waveform code, change to function component

--- src/components/Visualizer/Visualizations/Waveform/Waveform.tsx
+++ src/components/Visualizer/Visualizations/Waveform/Waveform.tsx
@@ -4,55 +4,34 @@ import Canvas from './Canvas';
 import * as Visualization from '../Visualization';
 import './Waveform.scss';
 
-const MAX_SIMULTANEOUS_WAVEFORMS = 5;
+// we are always generating a new canvas for each
+// data and then throwing it out, so this id exists
+// very ephemerally
 let id = 0;
 
-class Waveform extends React.Component<Visualization.WrappedProps> {
-  // state: State = {
-  //   waveforms: []
-  // };
+const Waveform: React.FunctionComponent<Visualization.WrappedProps> = ({ data, style }) => {
+  id += 1;
 
-  // static getDerivedStateFromProps(props: Visualization.WrappedProps, state: State) {
-  //   const { data } = props;
-  //   const { waveforms } = state;
-
-  //   const nextWaveform = {
-  //     id,
-  //     data
-  //   };
-
-  //   id += 1;
-  //   console.log('id', id);
-
-  //   const nextWaveforms = waveforms.concat(nextWaveform);
-
-  //   return {
-  //     waveforms: nextWaveforms.length > MAX_SIMULTANEOUS_WAVEFORMS ? nextWaveforms.slice(1) : nextWaveforms
-  //   };
-  // }
-
-  render() {
-    const { data, style } = this.props;
-
-    id += 1;
-
-    return (
-      <TransitionGroup className="visualization waveform" style={style}>
-        <CSSTransition
-          key={id}
-          timeout={{
-            enter: 150,
-            exit: 300
-          }}
-          classNames="fade"
-          unmountOnExit
-        >
-          <Canvas key={id} data={data} />
-        </CSSTransition>
-        ))}
-      </TransitionGroup>
-    );
+  if (id > 99) {
+    id = 0;
   }
-}
+
+  return (
+    <TransitionGroup className="visualization waveform" style={style}>
+      <CSSTransition
+        key={id}
+        timeout={{
+          enter: 150,
+          exit: 300
+        }}
+        classNames="fade"
+        unmountOnExit
+      >
+        <Canvas key={id} data={data} />
+      </CSSTransition>
+      ))}
+    </TransitionGroup>
+  );
+};
 
 export default Visualization.wrap(Waveform);
