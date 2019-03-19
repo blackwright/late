81877eead675aafa156b467663052dff4a763253
Jerry Gao <gao@blackwright.com>
2019-03-18 22:06:07 -0400

    use performant rendering strategy for Waveform canvas

--- src/components/Visualizer/Visualizations/Waveform/Waveform.tsx
+++ src/components/Visualizer/Visualizations/Waveform/Waveform.tsx
@@ -1,37 +1,97 @@
 import React from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import Canvas from './Canvas';
 import * as Visualization from '../Visualization';
+import { getRandomColorTriple } from '../../../../utils/colors';
 import './Waveform.scss';
 
-// we are always generating a new canvas for each
-// data and then throwing it out, so this id exists
-// very ephemerally
-let id = 0;
+const LINE_WIDTH = 10;
+const LINE_X_OFFSET = 0;
+const LINE_Y_OFFSET = 5;
 
-const Waveform: React.FunctionComponent<Visualization.WrappedProps> = ({ data, style }) => {
-  id += 1;
+class Waveform extends React.Component<Visualization.WrappedProps> {
+  private ref: React.RefObject<HTMLCanvasElement> = React.createRef();
+  private canvas?: HTMLCanvasElement;
+  private ctx?: CanvasRenderingContext2D;
+  private intervalId?: number;
+  private resizeTimeoutId?: number;
 
-  if (id > 99) {
-    id = 0;
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+
+    this.canvas = this.ref.current!;
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+
+    this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    this.intervalId = window.setInterval(this.fadeOut, 7);
   }
 
-  return (
-    <TransitionGroup className="visualization waveform" style={style}>
-      <CSSTransition
-        key={id}
-        timeout={{
-          enter: 150,
-          exit: 200
-        }}
-        classNames="fade"
-        unmountOnExit
-      >
-        <Canvas key={id} data={data} />
-      </CSSTransition>
-      ))}
-    </TransitionGroup>
-  );
-};
+  componentDidUpdate() {
+    this.paint();
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.intervalId);
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = () => {
+    window.clearTimeout(this.resizeTimeoutId);
+    const canvas = this.canvas!;
+
+    this.resizeTimeoutId = window.setTimeout(() => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }, 500);
+  };
+
+  paint = () => {
+    const { data } = this.props;
+    const canvas = this.canvas!;
+    const ctx = this.ctx!;
+
+    const colors = getRandomColorTriple();
+
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.lineCap = 'round';
+
+    const sliceWidth = canvas.width / data.length;
+
+    colors.forEach((color, i) => {
+      ctx.strokeStyle = color;
+      ctx.beginPath();
+
+      let x = i * LINE_X_OFFSET;
+      ctx.moveTo(x, canvas.height / 2);
+
+      data.forEach(dataElement => {
+        const y = (dataElement / 255.0) * canvas.height - (LINE_Y_OFFSET * colors.length) / 2;
+        ctx.lineTo(x, y + i * ((LINE_Y_OFFSET * colors.length) / 2));
+        x += sliceWidth;
+      });
+
+      ctx.lineTo(x, canvas.height / 2);
+      ctx.stroke();
+    });
+  };
+
+  fadeOut = () => {
+    const ctx = this.ctx!;
+    const canvas = this.canvas!;
+    ctx.fillStyle = 'rgba(4, 4, 4, 0.1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  };
+
+  render() {
+    const { style } = this.props;
+
+    return (
+      <div className="visualization waveform" style={style}>
+        <div className="backdrop" />
+        <canvas ref={this.ref} />;
+      </div>
+    );
+  }
+}
 
 export default Visualization.wrap(Waveform);
