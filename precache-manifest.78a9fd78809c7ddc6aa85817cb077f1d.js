b2d2768a284796747183e29a9737268514553d96
Jerry Gao <gao@blackwright.com>
2019-03-26 22:22:09 -0400

    Updates

---
+++ precache-manifest.78a9fd78809c7ddc6aa85817cb077f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc4187e479a5ba088cf3",
+    "url": "/ltly/static/js/runtime~main.bc4187e4.js"
+  },
+  {
+    "revision": "aa287f6c2f38815750b2",
+    "url": "/ltly/static/js/main.aa287f6c.chunk.js"
+  },
+  {
+    "revision": "0f8da56f22b87ed1537c",
+    "url": "/ltly/static/js/2.0f8da56f.chunk.js"
+  },
+  {
+    "revision": "aa287f6c2f38815750b2",
+    "url": "/ltly/static/css/main.99035eb4.chunk.css"
+  },
+  {
+    "revision": "84a785101a61bc3fada079e639d52026",
+    "url": "/ltly/index.html"
+  }
+];
