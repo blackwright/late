0deb23451c605b30a4769d45b3bd83808dc102f2
Jerry Gao <gao@blackwright.com>
2019-04-02 23:21:49 -0400

    Updates

---
+++ precache-manifest.3e15a5143206732f9c4200535b9ceacb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc4187e479a5ba088cf3",
+    "url": "/ltly/static/js/runtime~main.bc4187e4.js"
+  },
+  {
+    "revision": "27ded7951204b761c057",
+    "url": "/ltly/static/js/main.27ded795.chunk.js"
+  },
+  {
+    "revision": "5ca0a83c58786b795eb5",
+    "url": "/ltly/static/js/2.5ca0a83c.chunk.js"
+  },
+  {
+    "revision": "27ded7951204b761c057",
+    "url": "/ltly/static/css/main.99123b3b.chunk.css"
+  },
+  {
+    "revision": "bd26d293b07672deb9d3754dafe7d533",
+    "url": "/ltly/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ltly/precache-manifest.c8a9b5db160aa65c6abe89da493aa34f.js"
+  "/ltly/precache-manifest.3e15a5143206732f9c4200535b9ceacb.js"
 );
 
 workbox.clientsClaim();
