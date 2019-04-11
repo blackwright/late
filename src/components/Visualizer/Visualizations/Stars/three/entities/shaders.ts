1ee6892725352e43db75b52822b3672d37a143a7
Jerry Gao <gao@blackwright.com>
2019-04-10 22:14:53 -0400

    update star shaders to render round stars with size proportionate to alpha

--- src/components/Visualizer/Visualizations/Stars/three/entities/shaders.ts
+++ src/components/Visualizer/Visualizations/Stars/three/entities/shaders.ts
@@ -5,7 +5,7 @@ varying float vAlpha;
 void main() {
   vAlpha = alpha;
   vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-  gl_PointSize = 2.0;
+  gl_PointSize = 5.0 * alpha;
   gl_Position = projectionMatrix * mvPosition;
 }
 `;
@@ -15,6 +15,13 @@ uniform vec3 color;
 varying float vAlpha;
 
 void main() {
+  float r = 0.0;
+  vec2 cxy = 2.0 * gl_PointCoord - 1.0;
+  r = dot(cxy, cxy);
+  if (r > 1.0) {
+    discard;
+  }
+
   gl_FragColor = vec4(color, vAlpha);
 }
 `;
