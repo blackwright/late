68860c4ea9d1f974b7b1023239065fd833a21403
Jerry Gao <gao@blackwright.com>
2019-05-02 23:34:40 -0400

    update no web audio API message to be more descriptive

--- src/components/NoWebAudioApi/index.tsx
+++ src/components/NoWebAudioApi/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './NoWebAudioApi.scss';
 
 const NoWebAudioApi = () => (
-  <div id="no-web-audio">Your device doesn't support the Web Audio API.</div>
+  <div id="no-web-audio">
+    <p>Your device doesn't fully support the Web Audio API.</p>
+    <p>This site should be viewed on a non-mobile or Android device.</p>
+  </div>
 );
 
 export default NoWebAudioApi;
