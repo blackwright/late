f1b38a60ef14948733a14951e71edf3745c4e579
Jerry Gao <gao@blackwright.com>
2019-05-16 23:12:24 -0400

    add a bowl of fruit, why not

--- src/components/Visualizer/Visualizations/Rain/home/table.ts
+++ src/components/Visualizer/Visualizations/Rain/home/table.ts
@@ -3,26 +3,29 @@ import { roundedRect, CornerRadii } from '../utils';
 
 const WOOD_COLOR = '#122C2B';
 const TABLE_DECORATION_COLOR = '#06151A';
+const BOWL_COLOR = '#3e3e3e';
+const FRUIT_COLORS = ['#564837', '#713C3A'];
+const FRUIT_STEM_COLOR = '#04070F';
 
 export class Table extends Renderer {
+  y: number;
+
   constructor(ctx: CanvasRenderingContext2D) {
     super(ctx);
+
+    this.y = (this.canvasHeight * 5) / 6;
   }
 
-  render() {
-    super.render();
-
+  top() {
     const {
       ctx,
+      y,
       windowFrameThickness,
-      canvasHeight,
       oneThirdCanvasWidth,
       roundedRadius
     } = this;
 
-    // table top
     const width = oneThirdCanvasWidth * 1.1;
-    const y = (canvasHeight * 5) / 6;
     const thickness = windowFrameThickness * 1.35;
 
     const tableCornerRadii: CornerRadii = {
@@ -35,15 +38,6 @@ export class Table extends Renderer {
     ctx.fillStyle = WOOD_COLOR;
     roundedRect(ctx, 0, y, width, thickness, tableCornerRadii);
 
-    // table leg
-    ctx.strokeStyle = WOOD_COLOR;
-    ctx.lineWidth = windowFrameThickness * 2;
-    ctx.beginPath();
-    ctx.moveTo((oneThirdCanvasWidth * 2) / 3, y);
-    ctx.lineTo((oneThirdCanvasWidth * 2) / 3, canvasHeight);
-    ctx.stroke();
-
-    // decoration
     ctx.fillStyle = TABLE_DECORATION_COLOR;
     ctx.beginPath();
     ctx.rect(
@@ -53,5 +47,126 @@ export class Table extends Renderer {
       windowFrameThickness / 4
     );
     ctx.fill();
+  }
+
+  leg() {
+    const {
+      ctx,
+      y,
+      windowFrameThickness,
+      canvasHeight,
+      oneThirdCanvasWidth
+    } = this;
+
+    ctx.strokeStyle = WOOD_COLOR;
+    ctx.lineWidth = windowFrameThickness * 2;
+    ctx.beginPath();
+    ctx.moveTo((oneThirdCanvasWidth * 2) / 3, y);
+    ctx.lineTo((oneThirdCanvasWidth * 2) / 3, canvasHeight);
+    ctx.stroke();
+  }
+
+  centerpiece() {
+    const {
+      ctx,
+      y,
+      windowFrameThickness,
+      canvasWidth,
+      oneThirdCanvasWidth
+    } = this;
+
+    const bowlCenterX = oneThirdCanvasWidth * 0.25;
+    const bowlWidth = oneThirdCanvasWidth * 0.3;
+    const bowlHeight = windowFrameThickness * 2;
+
+    // fruit
+    const fruits = [
+      {
+        xOffset: (bowlWidth - bowlWidth * 0.4) / 4,
+        y: y - bowlHeight,
+        radius: bowlWidth * 0.2,
+        color: FRUIT_COLORS[0],
+        stemAngle: Math.PI * 0.9
+      },
+      {
+        xOffset: -(bowlWidth - bowlWidth * 0.4) / 4,
+        y: y - bowlHeight + bowlHeight / 8,
+        radius: bowlWidth * 0.2,
+        color: FRUIT_COLORS[1],
+        stemAngle: Math.PI * 1.1
+      }
+    ];
+
+    for (const fruit of fruits) {
+      ctx.fillStyle = fruit.color;
+      ctx.beginPath();
+      ctx.arc(
+        bowlCenterX + fruit.xOffset,
+        fruit.y,
+        fruit.radius,
+        0,
+        Math.PI * 2
+      );
+      ctx.fill();
+    }
+
+    ctx.strokeStyle = FRUIT_STEM_COLOR;
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 2;
+
+    for (const fruit of fruits) {
+      ctx.save();
+      ctx.translate(bowlCenterX + fruit.xOffset, fruit.y);
+      const stemStart = {
+        x: Math.sin(fruit.stemAngle) * fruits[1].radius * 0.9,
+        y: Math.cos(fruit.stemAngle) * fruits[1].radius * 0.9
+      };
+      const stemEnd = {
+        x: Math.sin(fruit.stemAngle) * fruits[1].radius * 1.1,
+        y: Math.cos(fruit.stemAngle) * fruits[1].radius * 1.1
+      };
+      ctx.beginPath();
+      ctx.moveTo(stemStart.x, stemStart.y);
+      ctx.lineTo(stemEnd.x, stemEnd.y);
+      ctx.stroke();
+      ctx.restore();
+    }
+
+    // bowl
+    const bowlCornerRadii: CornerRadii = {
+      tl: 0,
+      tr: 0,
+      br: oneThirdCanvasWidth * 0.1,
+      bl: oneThirdCanvasWidth * 0.1
+    };
+
+    ctx.fillStyle = BOWL_COLOR;
+    roundedRect(
+      ctx,
+      bowlCenterX - bowlWidth / 2,
+      y - bowlHeight,
+      bowlWidth,
+      bowlHeight,
+      bowlCornerRadii
+    );
+
+    ctx.save();
+    ctx.clip();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = TABLE_DECORATION_COLOR;
+    const bowlDecorationY = y - bowlHeight + bowlHeight / 6;
+    ctx.beginPath();
+    ctx.moveTo(0, bowlDecorationY);
+    ctx.lineTo(canvasWidth, bowlDecorationY);
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  render() {
+    super.render();
+
+    this.leg();
+    this.top();
+    this.centerpiece();
   }
 }
